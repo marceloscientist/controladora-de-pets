b61f367eaa8b0feada781df4a77faaae4760d197
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2022-06-17 15:25:16 -0300

    Fixing Swagger pom

--- src/main/java/com/qintess/controladoradepets/controller/PetController.java
+++ src/main/java/com/qintess/controladoradepets/controller/PetController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/pets")
+//@RequestMapping("/api/v1/pets")
 public class PetController {
-
-
     @Autowired
     private PetService petService;
 
